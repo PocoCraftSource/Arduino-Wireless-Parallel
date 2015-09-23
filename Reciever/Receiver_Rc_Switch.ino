f3ec8fff2c042bb27ce704e6e8b9d3e22f6c4077
Coleton <coleton@cole-nix>
2015-09-23 14:53:46 -0500

    Added Transmitter and Reciever code

---
+++ Reciever/Receiver_Rc_Switch.ino
@@ -0,0 +1,22 @@
+#include <RCSwitch.h>
+
+#define data 2
+#define clock 3
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  pinMode(clock, OUTPUT);
+  pinMode(data, OUTPUT);
+  mySwitch.enableReceive(4);
+  mySwitch.setProtocol(2);
+  shiftOut(data, clock, LSBFIRST, B00000000);
+}
+
+void loop() {
+  if (mySwitch.available()) {
+    unsigned long res = mySwitch.getReceivedValue();
+    shiftOut(data, clock, MSBFIRST, res);
+    mySwitch.resetAvailable();
+  }
+}
---
+++ Transmitter/Transmitter_Rc_Switch.ino
@@ -0,0 +1,33 @@
+// Pin connections for Uno and similar:
+// Chip pin 1 (/PL)  goes to LATCH (D9)  (or any other pin by changing LATCH below)
+// Chip pin 2 (CP)   goes to SCK   (D13)
+// Chip pin 9 (Q7)   goes to MISO  (D12)
+// Pin connections for Mega2560:
+// Chip pin 1 (/PL)  goes to LATCH (D9)  (or any other pin by changing LATCH below)
+// Chip pin 2 (CP)   goes to SCK   (D52)
+// Chip pin 9 (Q7)   goes to MISO  (D50)
+
+#include <RCSwitch.h>
+#include <SPI.h>
+
+const byte LATCH = 9;
+byte optionSwitch;
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  mySwitch.enableTransmit(46);
+  mySwitch.setPulseLength(0); // Was 1
+  mySwitch.setProtocol(2); // Was 1
+  mySwitch.setRepeatTransmit(1); // Should be 2?
+  SPI.begin();
+  pinMode(LATCH, OUTPUT);
+  digitalWrite(LATCH, HIGH);
+}
+
+void loop() {
+  digitalWrite(LATCH, LOW);
+  digitalWrite(LATCH, HIGH);
+  optionSwitch = SPI.transfer(0);
+  mySwitch.send(optionSwitch, 24);
+}
